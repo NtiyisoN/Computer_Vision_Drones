b6776c5fbcd5ae9685fe1a42c17fb917404e38c3
zsmoore <zsmoore@buffalo.edu>
2016-10-31 13:11:01 -0400

    added a ROS workspace for dji development -- (located in catkin_ws)

---
+++ drone_signal/catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gtest
+# Build directory: /home/zsmoore/programming/CSE321/Computer_Vision_Drones/drone_signal/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
